cabd6b614032bb97f851c698b9655ff105d7f120
Jonas Olofsson <jonas_emailbox@yahoo.co.uk>
2019-05-03 20:54:19 -0300

    0.0.2

--- index.js
+++ index.js
@@ -3,7 +3,6 @@
 var os = require('os');
 var fs = require('fs');
 var path = require('path');
-
 var verifyFile = require('./lib/verify-file');
 
 var platform = os.platform() + '-' + os.arch();
@@ -11,37 +10,57 @@ var platform = os.platform() + '-' + os.arch();
 var packageName = '@ffmpeg-installer/' + platform;
 
 if (!require('./package.json').optionalDependencies[packageName]) {
-    throw 'Unsupported platform/architecture: ' + platform;
+  throw 'Unsupported platform/architecture: ' + platform;
 }
 
-var binary = os.platform() === 'win32' ? 'ffmpeg.exe' : 'ffmpeg';
+function calcPath(dir) {
+  var binary = os.platform() === 'win32' ? 'ffmpeg.exe' : 'ffmpeg';
 
-var npm3Path = path.resolve(__dirname, '..', platform);
-var npm2Path = path.resolve(__dirname, 'node_modules', '@ffmpeg-installer', platform);
+  var npm3Path = path.resolve(__dirname, '..', platform);
+  var npm2Path = path.resolve(
+    __dirname,
+    'node_modules',
+    '@ffmpeg-installer',
+    platform,
+  );
 
-var npm3Binary = path.join(npm3Path, binary);
-var npm2Binary = path.join(npm2Path, binary);
+  var npm3Binary;
+  var npm2Binary;
+  if (dir) {
+    npm3Binary = path.join(npm3Path, dir, binary);
+    npm2Binary = path.join(npm2Path, dir, binary);
+  } else {
+    npm3Binary = path.join(npm3Path, binary);
+    npm2Binary = path.join(npm2Path, binary);
+  }
 
-var npm3Package = path.join(npm3Path, 'package.json');
-var npm2Package = path.join(npm2Path, 'package.json');
+  var npm3Package = path.join(npm3Path, 'package.json');
+  var npm2Package = path.join(npm2Path, 'package.json');
 
-var ffmpegPath, packageJson;
+  var ffmpegPath, packageJson;
 
-if (verifyFile(npm3Binary)) {
+  if (verifyFile(npm3Binary)) {
     ffmpegPath = npm3Binary;
     packageJson = require(npm3Package);
-} else if (verifyFile(npm2Binary)) {
+  } else if (verifyFile(npm2Binary)) {
     ffmpegPath = npm2Binary;
     packageJson = require(npm2Package);
-} else {
-    throw 'Could not find ffmpeg executable, tried "' + npm3Binary + '" and "' + npm2Binary + '"';
-}
+  } else {
+    throw 'Could not find ffmpeg executable, tried "' +
+      npm3Binary +
+      '" and "' +
+      npm2Binary +
+      '"';
+  }
 
-var version = packageJson.ffmpeg || packageJson.version;
-var url = packageJson.homepage;
+  var version = packageJson.ffmpeg || packageJson.version;
+  var url = packageJson.homepage;
 
-module.exports = {
+  return {
     path: ffmpegPath,
     version: version,
-    url: url
-};
+    url: url,
+  };
+}
+
+module.exports = calcPath;
